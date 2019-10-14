6a02d9ce33ac294a4777c9331b143280381b4184
Joe <yogurmystery@gmail.com>
2019-10-14 18:15:06 -0500

    spiral matrix and fibonacci -featuring two recursive solutions, and one iterative

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// iterative solution
+// function fib(n) {
+//   let fibs = [0, 1];
+//   if (n > 1) {
+//     for (let i = 2; i <= n; i++) {
+//       fibs.push(fibs[i - 2] + fibs[i - 1]);
+//     }
+//   }
+//   return fibs[n];
+// }
+
+// recursive solution
+let fibs = [0, 1];
+function fib(n) {
+  if (fibs.length < n + 1) {
+    fibs.push(fibs[fibs.length-1] + fibs[fibs.length - 2]);
+    return fib(n);
+  } else {
+    return fibs[n];
+  }
+}
+
+// alternate recursive solution
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 module.exports = fib;
+
